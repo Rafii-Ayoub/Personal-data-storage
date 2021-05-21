b27856fd52636c5d70e9630137f7582d372e0f6e
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-05-21 21:48:33 +0200

    Create SystemConnexion.java

---
+++ src/SystemConnexion.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class SystemConnexion {
+	
+	private SystemNode sysNode1;
+	private SystemNode sysNode2;
+	
+	public SystemConnexion(SystemNode node1,SystemNode node2) {
+		this.sysNode1 = node1;
+		this.sysNode2 =node2;
+	}
+	
+	public ArrayList<SystemNode> getNodes() {
+	    ArrayList<SystemNode> list = new ArrayList<>();
+	    list.add(this.getFirstNode());
+	    list.add(this.getSecondNode());
+	    return list ;
+	    
+	}
+
+	public SystemNode getFirstNode() {
+		return sysNode1;
+	}
+
+	public void setFirstNode(SystemNode sysNode1) {
+		this.sysNode1 = sysNode1;
+	}
+
+	public SystemNode getSecondNode() {
+		return sysNode2;
+	}
+
+	public void setSecondNode(SystemNode sysNode2) {
+		this.sysNode2 = sysNode2;
+	}
+	
+	
+
+
+}
