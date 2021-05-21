8628c3d487ff8b765a3b6ab111c121b049bee71c
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-05-21 21:47:41 +0200

    Update Data.java

--- src/Data.java
+++ src/Data.java
@@ -1,1 +1,34 @@
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class Data {
+	
+	private static final AtomicInteger count = new AtomicInteger(1000000000); 
+	private int size;	
+	private String subject;
+	private int id;
+  
+	
+	Data(int size, String subject) {
+
+        this.id =count.incrementAndGet();
+		this.size = size;
+		this.subject = subject;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
+
+}
