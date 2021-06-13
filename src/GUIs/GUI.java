cfad58fb16302d6b3d67a788ab00b0706e97734d
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-06-13 04:59:39 +0200

    In progress

--- src/GUIs/GUI.java
+++ src/GUIs/GUI.java
@@ -1,1 +1,89 @@
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+
+/*
+public class GUI extends JFrame {
+		  
+		  private JPanel pan = new JPanel();
+		  
+		 
+		  
+		  private JButton bouton2 = new JButton("");
+		  public static String onlineUsers2 ="";
+		  
+		  private JPanel container = new JPanel();
+		  private JLabel label = new JLabel("");
+		  private String text;
+		  private JTextField textField = new JTextField(20);
+		  private JTextField textField2 = new JTextField(20);  
+		  private SystemTree tree;
+		  
+		  
+		  public GUI(SystemTree tree){
+		    this.setTitle("");
+		    this.setSize(500, 300);
+		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    this.setLocationRelativeTo(null); 
+		    this.tree =tree;
+		    this.setResizable(false);
+
+		    
+		    container.setBackground(Color.white);
+		    container.setLayout(new BorderLayout());
+		    
+		    
+		    container.add(pan, BorderLayout.CENTER);
+		    
+		    
+		    JLabel label1 = new JLabel("Data ");
+		    pan.add(label1);
+		    textField.setToolTipText("Saisissez le sujet");
+		    pan.add(textField);
+		    
+		    JLabel label2 = new JLabel("");
+		    pan.add(label2);
+		    
+		    textField2.setToolTipText("");
+		    pan.add(textField2);
+		   
+		    pan.add(bouton2, BorderLayout.CENTER);  
+		    
+		    container.add(pan, BorderLayout.CENTER);
+		    
+		    
+		    Font police = new Font("Tahoma", Font.BOLD, 16);
+		    label.setFont(police);  
+		    label.setForeground(Color.blue); 
+		    label.setHorizontalAlignment(JLabel.CENTER);
+		    container.add(label, BorderLayout.NORTH);
+		    
+		    
+		    this.setContentPane(container);
+		    this.setVisible(true);
+		    
+		    
+		    
+		    bouton2.addActionListener(new ActionListener() {
+		    	public void actionPerformed(ActionEvent e) {
+		    		
+		    	
+		    });
+		    
+		  }
+}
+*/
