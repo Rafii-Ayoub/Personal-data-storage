c713dcf68b1ec5f31b6d076d289dac83dfc5f6bb
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-06-13 04:42:15 +0200

    Update DataDB.java

--- src/System/DataDB.java
+++ src/System/DataDB.java
@@ -1,38 +1,77 @@
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.Objects;
 
-public class Data implements Comparable<Data> {
-	
-	private static final AtomicInteger count = new AtomicInteger(1000000000); 
-	private int size;	
-	private String subject;
-	private int id;
-  
-	
-	Data(int size, String subject) {
+public class DataDB {
 
-        this.id =count.incrementAndGet();
-		this.size = size;
-		this.subject = subject;
+    // Attributes	
+	
+	private ArrayList<Data> dataDB ;
+	
+	/**
+	 * Constructor
+	 */
+	
+	public DataDB () {
+		this.dataDB = new ArrayList<>();
 	}
 	
-	public int compareTo(Data o) {
-	    return Integer.compare(getId(),o.getId());
-	  }
 	
-	public int getId() {
-		return id;
-	}
-
 	
-	public int getSize() {
-		return size;
+	// Methods and functions
+	
+	/**
+	 * method to find a Data object by its id
+	 * @param data_id 
+	 * @return Data object 
+	 */
+
+	public Data findData(int data_id) {
+		Data result= null;
+		for ( int i=0; i < dataDB.size(); i++ ) {
+			Data element = dataDB.get(i);
+			int id = element.getId();
+			if(data_id== id) 
+				result =  element;
+			    break;
+			
+		}
+		return result ;
+	}
+	
+	/**
+	 * method to add object data to the DataDb list
+	 * @param data
+	 */
+	public void addData(Data data){
+		dataDB.add(data);
+	}
+	
+	/**
+	 * method to add data by data id
+	 * @param id_data
+	 */
+	
+	public void addData(int id_data) {
+		Data element = findData(id_data);
+		if (element == null ){
+			// do nothing
+		}
+		else {
+			dataDB.add(element);
+		}
 	}
 
-	public void setSize(int size) {
-		this.size = size;
+
+	// getters and setters 
+	
+	public ArrayList<Data> getDataDB() {
+		return dataDB;
 	}
 
+	public void setDataDB(ArrayList<Data> dataDB) {
+		this.dataDB = dataDB;
+	}
+	
 
 
 }
